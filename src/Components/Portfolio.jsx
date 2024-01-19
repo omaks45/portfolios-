fbbdaac86eaa18d784a54b515341170238343d41
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2024-01-19 22:17:55 +0000

    update

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/developer.jpg";
+import image from "../images/omaks.jpg";
 
-const imageAltText = "software developer working on a laptop";
+const imageAltText = "software developer";
 
 /**
  * Project list
